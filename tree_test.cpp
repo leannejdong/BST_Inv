97e4387859ea39e8568c7c789e7c6f20230a9de9
Leanne Dong <jdleanne@gmail.com>
2020-10-04 01:03:55 -0400

    test passed

--- tree_test.cpp
+++ tree_test.cpp
@@ -6,22 +6,22 @@ int main()
 {
     nTree number = 10;
     nTree *root = &number;
-//
-//    // unique_ptr<nTree> root = newNode(10);
-//    (root->child).push_back(newNode(2));
-//    (root->child).push_back(newNode(34));
-//    (root->child).push_back(newNode(56));
-//    (root->child).push_back(newNode(100));
-//    (root->child[0]->child).push_back(newNode(77));
-//    (root->child[0]->child).push_back(newNode(88));
-//    (root->child[2]->child).push_back(newNode(1));
-//    (root->child[3]->child).push_back(newNode(7));
-//    (root->child[3]->child).push_back(newNode(8));
-//    (root->child[3]->child).push_back(newNode(9));
-//
-//    cout << "Level order traversal Before Mirroring\n";
 
-  //  invertnTree(root);
+    // unique_ptr<nTree> root = newNode(10);
+    (root->child).push_back(newNode(2));
+    (root->child).push_back(newNode(34));
+    (root->child).push_back(newNode(56));
+    (root->child).push_back(newNode(100));
+    (root->child[0]->child).push_back(newNode(77));
+    (root->child[0]->child).push_back(newNode(88));
+    (root->child[2]->child).push_back(newNode(1));
+    (root->child[3]->child).push_back(newNode(7));
+    (root->child[3]->child).push_back(newNode(8));
+    (root->child[3]->child).push_back(newNode(9));
+
+    cout << "Level order traversal Before Mirroring\n";
+
+    invertnTree(root);
 
     ostringstream stream;
     nonRecursiveTraversal(root, stream);
